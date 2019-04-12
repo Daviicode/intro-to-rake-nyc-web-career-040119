37d1640ae5ffad2050f1ddafed5a22e8eb7c719d
Daviicode <d.ydzdavid@yahoo.com>
2019-04-12 08:54:26 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-require_relative "../lib/student.rb"
 
-Student.create(name: "Melissa", grade: "10th")
-Student.create(name: "April", grade: "10th")
-Student.create(name: "Luke", grade: "9th")
-Student.create(name: "Devon", grade: "11th")
-Student.create(name: "Sarah", grade: "10th")
+  require_relative "../lib/student.rb"
+
+  Student.create(name: "Melissa", grade: "10th")
+  Student.create(name: "April", grade: "10th")
+  Student.create(name: "Luke", grade: "9th")
+  Student.create(name: "Devon", grade: "11th")
+  Student.create(name: "Sarah", grade: "10th")
+
+
+  # namespace :db do
+  #
+  #   ...
+  #
+  #   desc 'seed the database with some dummy data'
+  #   task :seed do
+  #     require_relative './db/seeds.rb'
+  #   end
